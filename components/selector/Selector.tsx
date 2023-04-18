69247c7bb4c84a9f009d730f5c965d8211b56b69
taehee <jangth0655@gmail.com>
2023-04-19 08:48:48 +0900

    feat : 회원가입 - 휴대폰, 이메일 selector box 토글기능 업데이트

--- components/selector/Selector.tsx
+++ components/selector/Selector.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cls } from '@/utils/cls';
-import { Dispatch, SetStateAction, useContext, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
 interface Props {
@@ -27,29 +27,31 @@ const Selector = ({
     setIsActive((prev) => !prev);
   };
 
-  const handleValue = (number: string) => {
-    setValue(number);
+  const handleSetValue = (
+    e: React.MouseEvent<HTMLDivElement>,
+    value: string
+  ) => {
+    e.stopPropagation();
     setIsActive(false);
+    setValue(value);
   };
 
   return (
     <div
+      onClick={handleActive}
       className={cls(
         'px-2 py-1 rounded-md bg-transparent border-2 border-gray-200 text-gray-400 relative cursor-pointer',
         width || ''
       )}
     >
       {value}
-      <div
-        onClick={handleActive}
-        className='absolute right-1 top-[50%] -translate-y-1/2 text-white cursor-pointer'
-      >
+      <div className='absolute right-1 top-[50%] -translate-y-1/2 text-white cursor-pointer'>
         <MdKeyboardArrowDown className='w-5 h-5' />
       </div>
 
       <div
         className={cls(
-          'absolute px-2 py-1 rounded-md bg-gray-100 text-gray-400 -left-1 top-10 transition-all origin-top-left z-10 overflow-auto',
+          'absolute px-2 py-1 rounded-md bg-gray-100 text-gray-400 -left-1 top-10 transition-all origin-top-left z-10 overflow-auto overflow-y-scroll',
           width || '',
           isActive ? 'scale-y-1' : 'scale-y-0',
           height || ''
@@ -57,14 +59,16 @@ const Selector = ({
       >
         {boardItems.map((item) => (
           <div
-            onClick={() => handleValue(item)}
+            onClick={(e: React.MouseEvent<HTMLDivElement>) =>
+              handleSetValue(e, item)
+            }
             className={cls(
               'p-1 text-gray-800 rounded-md text-sm',
               value === item ? 'bg-sky-100' : ''
             )}
             key={item}
           >
-            <span className=''>{item}</span>
+            <span>{item}</span>
           </div>
         ))}
       </div>
